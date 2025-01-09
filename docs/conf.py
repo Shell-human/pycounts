b0c84b7a328d70417159c5d79cb3648508714f8c
Shell Ou <aa224689@gmail.com>
2025-01-09 05:39:36 -0800

    successfully make html

--- docs/conf.py
+++ docs/conf.py
@@ -34,3 +34,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+
+autoapi_ignore = [
+    "**/.ipynb_checkpoints",  # 忽略所有检查点文件夹
+    "**/__pycache__",         # 忽略缓存文件夹
+]
